a4de86c2d5154d655654c61ae8b906a823712501
Alain K <alain.kahwaji@gmail.com>
2015-05-06 10:13:10 -0700

    attempt randomize

--- 2PlayerGame/main.c
+++ 2PlayerGame/main.c
@@ -7,9 +7,31 @@
 //
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    
+    srand(time(NULL));
+    int r = rand(20);
+    printf("%d\n", r);
+    
+    int a=12;
+    int b=16;
+    printf("Player 1: What does %d plus %d equal\n", a, b);
+    
+
+    int playerOneAnswer[2];
+    scanf("%d\n", playerOneAnswer);
+    
+    int playerTwoAnswer[2];
+    scanf("%d\n", playerTwoAnswer);
+    
+    // a way to ensure only integers are inputted
+    
+    
+    
+    
     return 0;
 }
